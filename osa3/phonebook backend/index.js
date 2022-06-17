949f935fe0c655dc283b78fc089550e1cbc8b44f
JoonasMV <102919507+JoonasMV@users.noreply.github.com>
2022-06-17 09:40:51 +0300

    3.11

--- osa3/phonebook backend/index.js
+++ osa3/phonebook backend/index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
 app.use(cors())
+app.use(express.static("build"))
 
 morgan.token("data", (req, res) => {
   return req.method === "POST" ? JSON.stringify(req.body) : " "
