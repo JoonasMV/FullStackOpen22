eb37515992124e1dddd962ac558a4d033a3b8ce9
JoonasMV <102919507+JoonasMV@users.noreply.github.com>
2022-06-27 20:12:37 +0300

    3.17

--- osa3/phonebook backend/index.js
+++ osa3/phonebook backend/index.js
@@ -69,6 +69,21 @@ app.post('/api/persons', (req, res) => {
   .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
 
