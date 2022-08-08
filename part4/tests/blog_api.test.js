71f97f1f4c6a5e308ac147519702129f1ff8b203
JoonasMV <102919507+JoonasMV@users.noreply.github.com>
2022-08-08 10:34:54 +0300

    4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -4,6 +4,7 @@ const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blogModel");
+const e = require("express");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
@@ -21,16 +22,33 @@ describe("blog api tests", () => {
   });
 
   test("id field is id", async () => {
-    const blogs = await api.get("/api/blogs")
-    console.log(blogs.body)
-    blogs.body.forEach(blog => {
-      expect(blog).toHaveProperty("id")
+    const blogs = await api.get("/api/blogs");
+    console.log(blogs.body);
+    blogs.body.forEach((blog) => {
+      expect(blog).toHaveProperty("id");
     });
-  })
+  });
+
+  test("blogs can be added", async () => {
+    const newBlog = new Blog({
+      title: "test",
+      author: "penis :D",
+      url: "www",
+      likes: 420,
+    });
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+    expect(response.body.length).toBe(helper.initialBlogs.length + 1);
+  });
 
   
 });
-
 
 afterAll(() => {
   mongoose.connection.close();
