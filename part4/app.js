ef70ec6d5a3202c0425248562f78126292f8b812
JoonasMV <102919507+JoonasMV@users.noreply.github.com>
2022-08-09 10:42:13 +0300

    4.18

--- part4/app.js
+++ part4/app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 
 const blogRouter = require("./controllers/blogRouter");
 const userRouter = require("./controllers/userRouter");
+const loginRouter = require("./controllers/loginRouter");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
