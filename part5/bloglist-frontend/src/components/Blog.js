84b01fd87f572ae939ea1be16e3170c4a1c85fd0
JoonasMV <102919507+JoonasMV@users.noreply.github.com>
2022-08-19 14:24:58 +0300

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useReducer, useState } from "react";
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState([])
+
+  const showMin = {display: visible ? "none" : "" }
+  const showInfo = { display: visible ? "" : "none" }
+
+  const toggleShown = () => {
+    setVisible(!visible)    
+  }
+  console.log(blog)
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <div style={showMin}>
+        <div>{blog.url}</div>
+        <div>Likes: {blog.likes} <button>Like</button></div>
+        <div>{blog.user.name}</div>
+      </div>
+      <button onClick={toggleShown}>Show</button>
+    </div>
+  );
+};
 
 export default Blog;
