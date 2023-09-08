05f6930ddf9ca2e9a3314387a759b4a708047d71
Joonas V <JoonasMRViljanen@gmail.com>
2023-09-08 20:39:39 +0300

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,18 +1,34 @@
-import { useQuery } from "@apollo/client"
-import { ALL_BOOKS } from "../queries"
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries";
+import { useState } from "react";
 
 const Books = (props) => {
-  const result = useQuery(ALL_BOOKS)
-  
+  const [genre, setGenre] = useState("");
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
+  const result = useQuery(ALL_BOOKS);
+
   if (!props.show) {
-    return null
+    return null;
   }
-  
+
   if (result.loading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
-  
-  const books = result.data.allBooks
+
+  const books = result.data.allBooks;
+
+  const genres = books.map((b) => b.genres).flat();
+  const uniqueGenres = genres.filter(
+    (value, index, array) => array.indexOf(value) === index
+  );
+
+  const filterByGenre = (genre) => {
+    if(!genre) {
+      return setFilteredBooks(books);
+    }
+    setFilteredBooks(() => books.filter(b => b.genres.includes(genre)))
+  }
 
   return (
     <div>
@@ -25,17 +41,25 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {filteredBooks
+            // .filter((b) => b.genres.includes(genre))
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      {uniqueGenres.map((genre) => (
+        <button onClick={() => filterByGenre(genre)} key={genre}>
+          {genre}
+        </button>
+      ))}
+      <button onClick={() => filterByGenre("")}>all genres</button>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
