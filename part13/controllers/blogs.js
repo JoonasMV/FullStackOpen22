327f3b6c4e96a53ff565a7ffbde65dd785ec02d0
Joonas <joonasmrviljanen@gmail.com>
2024-04-09 12:51:02 +0300

    13.7

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -2,27 +2,37 @@ const router = require("express").Router()
 
 const { Blog } = require("../models")
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll()
   return res.send(blogs)
 });
 
 router.post("/", async (req, res) => {
-  try {
     const blog = await Blog.create(req.body)
     return res.json(blog)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
 });
 
-router.delete("/:id", async (req, res) => {
-  await Blog.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
+router.put("/:id", blogFinder, async (req, res) => {
+  const blog = req.blog
+  if (!blog) {
+    return res.status(404).send({ error: "Blog not found" })
+  }
 
+    blog.likes++
+    await blog.save()
+    return res.json({ likes: blog.likes })
+  }
+)
+
+router.delete("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+  }
   return res.sendStatus(200)
 });
 
--- part13/index.js
+++ part13/index.js
@@ -1,20 +1,36 @@
 const express = require("express");
+require("express-async-errors");
 const app = express();
 
 const { PORT } = require("./util/config");
-const { connectToDatabase } = require("./util/db")
+const { connectToDatabase } = require("./util/db");
 
-const blogRouter = require("./controllers/blogs")
+const blogRouter = require("./controllers/blogs");
 
-app.use(express.json())
+const errorHandler = (error, req, res, next) => {
+  if (error.name === "SequelizeValidationError") {
+    const message = [];
 
-app.use("/api/blogs", blogRouter)
+    error.errors.forEach((err) => {
+      message.push(err.message);
+    });
+
+    return res.status(400).send({ error: message });
+  }
+
+  next(error)
+};
+
+app.use(express.json());
+
+app.use("/api/blogs", blogRouter);
+app.use(errorHandler);
 
 const start = async () => {
-  await connectToDatabase()
+  await connectToDatabase();
   app.listen(PORT, () => {
-    console.log("Server running on port ", PORT)
-  })
-}
+    console.log("Server running on port ", PORT);
+  });
+};
 
-start()
+start();
