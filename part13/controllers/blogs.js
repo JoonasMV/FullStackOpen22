0895cb0038dfd631ec20c1a1f8b1e54f5365ebb4
Joonas <joonasmrviljanen@gmail.com>
2024-04-15 12:15:26 +0300

    13.22

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -1,26 +1,9 @@
 const { Op } = require("sequelize");
 const router = require("express").Router();
-const jwt = require("jsonwebtoken");
+const { tokenExtractor } = require("../util/middleware");
 
 const { Blog, User } = require("../models");
 
-const { SECRET } = require("../util/config");
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      console.log(authorization.substring(7));
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch (error) {
-      console.log(error);
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
 
 const blogFinder = async (req, res, next) => {
   console.log(req.params.id);
--- part13/controllers/readingList.js
+++ part13/controllers/readingList.js
@@ -1,4 +1,5 @@
-const { ReadingList } = require("../models");
+const { ReadingList, User } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 const readingsRouter = require("express").Router();
 
@@ -9,6 +10,22 @@ readingsRouter.post("/", async (req, res) => {
     userId,
   });
   res.json(ret);
+});
+
+readingsRouter.put("/:id", tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const readingList = await ReadingList.findByPk(req.params.id);
+
+  if (!(user.id === readingList.userId)) {
+    return res
+      .status(403)
+      .json({ error: "You can only manage your own reading list" });
+  }
+
+  const updatedReadingList = await readingList.update({
+    read: req.body.read
+  })
+  res.json(updatedReadingList)
 });
 
 module.exports = readingsRouter;
